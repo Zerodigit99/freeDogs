0d756c8c52d7635d350d10e39294e4b87c098358
Zerodigit99 <zerodigit01@gmail.com>
2024-11-09 00:52:36 +0100

    Update memefi.py

--- memefi/memefi.py
+++ memefi/memefi.py
@@ -10,7 +10,7 @@ from urllib.parse import unquote
 import os
 print("Project root contents:", os.listdir("/opt/render/project/src"))
 print("memefi contents:", os.listdir("/opt/render/project/src/memefi"))
-from utils.headers import headers_set
+from memefi.utils.headers import headers_set
 from utils.queries import QUERY_USER, QUERY_LOGIN, MUTATION_GAME_PROCESS_TAPS_BATCH, QUERY_BOOSTER, QUERY_NEXT_BOSS
 from utils.queries import QUERY_TASK_VERIF, QUERY_TASK_COMPLETED, QUERY_GET_TASK, QUERY_TASK_ID, QUERY_GAME_CONFIG
 
